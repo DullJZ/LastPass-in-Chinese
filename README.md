fd69af08c0dd67ffd200f0acd94d2572eb5458ec
DullJZ <1768572730@qq.com>
2021-04-17 20:47:01 +0800

    2021.4.17更新，新增自动安装语言包功能

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # LastPass-in-Chinese
 # LastPass 汉化版
+> 2021.4.17 更新：新增自动安装语言包功能，再也不用繁琐的复制粘贴了！
 
+> 注意：现在LastPass已经把免费套餐改为移动端或电脑端二选一同步。也就是说，想要白嫖LastPass的电脑手机同步功能的可以下车了。
 ## 汉化原因
 
 1. 原本 LastPass 是有中文的，然而 苏州智务采信息技术有限公司 恶意抢注了中国区 LastPass 的商标，借此勒索钱财，LastPass 被迫退出中国市场，也因此取消了对中文的支持。~~真是比马克丁还马克丁~~
@@ -16,6 +18,16 @@
 </center>
 
 ## 汉化方法
+>注意：每一次更新LastPass扩展都需要汉化一次！更新会自动覆盖语言包！
+### 自动汉化（仅 Microsoft Edge 和 Google Chome）
+下载 release 中的 `auto-install.exe` ，按照提示操作即可。
+FireFox用户请看手动汉化流程
+
+### 手动汉化
+<details>
+<summary>展开查看</summary>
+<pre><code>
+
 ### Chromium 系浏览器
 1. 下载该存储库的 `messages.json` 文件
 2. 打开该路径：（以Edge Beta为例）
@@ -26,6 +38,8 @@
     ```
     C:\Users\你的用户名\AppData\Local\Microsoft\Edge Beta\User Data\Default\Extensions\bbcinlkgjjkejfdpemiealijmmooekmp\4.64.0.3_0\_locales\en_US
     ```
+
+    >当然也可以使用everything搜索浏览器扩展的ID
 3. 重命名该目录下的 `messages.json` 为其他名称
 4. 把下载的 `messages.json` 复制到该目录下
 5. 重启浏览器即可
@@ -60,6 +74,9 @@
     ![](https://cdn.jsdelivr.net/gh/jiangzhe11/MyPicture/Firefox%20last%20pass8.jpg)![](https://cdn.jsdelivr.net/gh/jiangzhe11/MyPicture/Firefox%20last%20pass9.jpg)
 12. 重启浏览器，问题解决！:)
     ![](https://cdn.jsdelivr.net/gh/jiangzhe11/MyPicture/Firefox%20last%20pass10.jpg)![](https://cdn.jsdelivr.net/gh/jiangzhe11/MyPicture/Firefox%20last%20pass11.jpg)
+
+</code></pre>
+</details>
 
 ## 翻译问题
 #### Note含有多个意思，如“笔记”、“注释”等，不予翻译，保留。
---
+++ auto-install/main.py
@@ -0,0 +1,51 @@
+import os
+import wget
+ID = ['bbcinlkgjjkejfdpemiealijmmooekmp', 'hdokiejnpimakedhajhdlcegeplioahd']
+browser = [
+    'Microsoft\\Edge', 'Microsoft\\Edge Beta', 'Microsoft\\Edge Dev',
+    'Microsoft\\Edge Canary', 'Google\\Chrome'
+]
+print('''================
+您使用的浏览器是：
+1. Microsoft Edge Chromium UWP
+2. Microsoft Edge Chromium Beta
+3. Microsoft Edge Chromium Dev
+4. Microsoft Edge Chromium Canary
+5. Google Chrome
+================
+''')
+option1 = int(input('输入序号：'))
+
+print('''================
+您的LastPass扩展是在哪里下载的：
+1. Microsoft Edge网上应用商店
+2. Chrome网上应用店
+================
+''')
+option2 = int(input('输入序号：'))
+
+path = os.getenv('LOCALAPPDATA') + '\\' + browser[
+    option1 - 1] + '\\User Data\\Default\\Extensions\\' + ID[option2 - 1]
+different_versions = os.listdir(path)
+
+print('===============')
+
+if len(different_versions) != 1:
+    print(r'发现不同版本的扩展，请手动安装！语言包目录在 \_locales\en_US 下')
+    for dirs in different_versions:
+        print(dirs)
+    os.system('explorer ' + path)
+else:
+    path = path + '\\' + different_versions[0] + '\\_locales\\en_US\\'
+    try:
+        if os.path.exists(path + 'messages.json.bak'):
+            os.remove(path + 'messages.json.bak')
+        os.rename(path + 'messages.json', path + 'messages.json.bak')
+        wget.download(
+            'https://cdn.jsdelivr.net/gh/DullJZ/LastPass-in-Chinese/messages.json',
+            out=path + 'messages.json')
+    except:
+        print('\n发生错误！')
+
+print('\n')
+exitting = input('按任意键退出')
